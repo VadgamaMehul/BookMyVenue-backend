104b229f57e0429d9e29bfb0f8efba78ef7f9950
Mehul Vadgama <vadgama.mehul444@gmail.com>
2024-06-27 22:32:41 +0530

    Update 6

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ export const config = {
 	API_VERSION_URL: '/api/v1',
 
 	SERVER: {
-		PORT: process.env.SERVER_PORT || 4000,
+		PORT: process.env.PORT || 4000,
 		ORIGINS: process.env.ORIGINS.split(','),
 	},
 	TOKEN: {
